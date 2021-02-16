4c912ac3f848112496f0ea5135466efb497325e5
dmitriy <yaroshdima01@gmail.com>
2021-02-16 16:40:15 +0200

    Create 2 file schema (stores all databases queries )

---
+++ shortener/schema.py
@@ -0,0 +1,16 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from .models import URL
+
+
+class URLType(DjangoObjectType):
+    class Meta:
+        model = URL
+
+
+class Query(graphene.ObjectType):
+    urls = graphene.List(URLType)
+
+    def resolve_urls(self, info, **kwargs):
+        return URL.objects.all()
